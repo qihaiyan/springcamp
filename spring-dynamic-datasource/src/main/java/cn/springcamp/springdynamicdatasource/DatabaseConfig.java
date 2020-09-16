db29f7f73adadcceb9c0e510fc05484f0ff2303b
qihaiyan <haiyan_qi@hotmail.com>
2020-09-16 15:23:11 +0800

    dynamic datasource

---
+++ spring-dynamic-datasource/src/main/java/cn/springcamp/springdynamicdatasource/DatabaseConfig.java
@@ -0,0 +1,50 @@
+package cn.springcamp.springdynamicdatasource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Data
+@Component
+@ConfigurationProperties(prefix = "dynamic-data")
+public class DatabaseConfig {
+    private List<DbSchema> shemas = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+        for (DbSchema current : this.getShemas()) {
+            HikariConfig jdbcConfig = new HikariConfig();
+            jdbcConfig.setJdbcUrl(current.getDatasource().getUrl());
+            jdbcConfig.setUsername(current.getDatasource().getUsername());
+            String password = current.getDatasource().getPassword();
+            jdbcConfig.setPassword(password);
+            try {
+                HikariDataSource hikariDataSource = new HikariDataSource(jdbcConfig);
+                current.setJdbcTemplate(new JdbcTemplate(hikariDataSource));
+            } catch (Exception e) {
+                log.error("connect to " + current.getDatasource().getUrl() + "  failed.");
+                throw e;
+            }
+        }
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class DbSchema {
+        private String code;
+        private DataSourceProperties datasource;
+        private String query;
+        private JdbcTemplate jdbcTemplate;
+    }
+}
---
+++ spring-dynamic-datasource/src/main/java/cn/springcamp/springdynamicdatasource/DemoApplication.java
@@ -0,0 +1,26 @@
+package cn.springcamp.springdynamicdatasource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private DatabaseConfig databaseConfig;
+
+    @Override
+    public void run(String... args) {
+        databaseConfig.getShemas().stream().filter(r -> !r.getQuery().isEmpty()).forEach(current -> {
+            String result = current.getJdbcTemplate().queryForObject(
+                    current.getQuery(), String.class);
+            System.out.println(current.getCode() + " content: " + result);
+        });
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
