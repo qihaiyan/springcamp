10532672bb5a04201e64d960526c0d77c7801568
qihaiyan <haiyan_qi@hotmail.com>
2021-01-17 13:36:37 +0800

    upgrade springboot to 2.4.2

--- spring-cache/src/test/java/cn/springcamp/springcache/DemoApplicationTests.java
+++ spring-cache/src/test/java/cn/springcamp/springcache/DemoApplicationTests.java
@@ -1,12 +1,10 @@
 package cn.springcamp.springcache;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
@@ -14,7 +12,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class DemoApplicationTests {
