319d4f92067ee22984c924b05b8ca3365b8f8d11
qihaiyan <haiyan_qi@hotmail.com>
2021-03-21 17:43:28 +0800

    fix lombok import

--- spring-aop/src/main/java/cn/springcamp/springaop/service/MyService.java
+++ spring-aop/src/main/java/cn/springcamp/springaop/service/MyService.java
@@ -2,8 +2,6 @@ package cn.springcamp.springaop.service;
 
 import cn.springcamp.springaop.aop.TrackTime;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Slf4j
