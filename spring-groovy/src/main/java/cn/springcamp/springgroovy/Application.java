0b4d62367584670b3c94c9f6cf330428421a7596
qihaiyan <haiyan_qi@hotmail.com>
2023-02-19 11:44:19 +0800

    add JSR-223 ScriptEngine for groovy

--- spring-groovy/src/main/java/cn/springcamp/springgroovy/Application.java
+++ spring-groovy/src/main/java/cn/springcamp/springgroovy/Application.java
@@ -8,6 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 @SpringBootApplication
 @ImportResource({"classpath:xml-bean-config.xml", "classpath:BeanBuilder.groovy"})
 public class Application implements CommandLineRunner {
@@ -24,12 +28,23 @@ public class Application implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws ScriptException, ResourceException, IllegalAccessException, InstantiationException {
+    public void run(String... args) throws ScriptException, ResourceException, IllegalAccessException, InstantiationException, javax.script.ScriptException, NoSuchMethodException {
         MyDomain myDomain = new MyDomain();
         myDomain.setName("test");
         System.out.println(myServiceXml.fun(myDomain));
         myDomain.setName("test2");
         System.out.println(myServiceGroovy.fun(myDomain));
         myEngine.runScript(1, 2);
+
+        ScriptEngineManager factory = new ScriptEngineManager();
+        ScriptEngine engine = factory.getEngineByName("groovy");
+        engine.put("first", "HELLO");
+        engine.put("second", "world");
+        String fact = "def fun(n) { first.toLowerCase() + ' ' + second.toUpperCase() + ' ' + n}";
+        engine.eval(fact);
+        Invocable inv = (Invocable) engine;
+        Object[] params = {"5"};
+        Object result = inv.invokeFunction("fun", params);
+        System.out.println("ScriptEngineManager result: " + result);
     }
 }
