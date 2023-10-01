1d06d5f47d669d7fd4fef17d649db97161cf97be
qihaiyan <haiyan_qi@hotmail.com>
2023-10-01 11:39:44 +0800

    log filter

---
+++ spring-rest-log-request-response/src/main/java/cn/springcamp/springlogrequestresponse/AccessLogFilter.java
@@ -0,0 +1,144 @@
+package cn.springcamp.springlogrequestresponse;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+import org.springframework.web.util.WebUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Component
+@WebFilter(filterName = "accessLogFilter", urlPatterns = "/*")
+public class AccessLogFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request,
+                                    final HttpServletResponse response,
+                                    final FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final boolean isFirstRequest = !isAsyncDispatch(request);
+
+        final boolean shouldWrapRequest = isFirstRequest && !(request instanceof ContentCachingRequestWrapper);
+        final HttpServletRequest requestToUse = shouldWrapRequest ? new ContentCachingRequestWrapper(request) : request;
+
+        final boolean shouldWrapResponse = !(response instanceof ContentCachingResponseWrapper);
+        final HttpServletResponse responseToUse = shouldWrapResponse ? new ContentCachingResponseWrapper(response) : response;
+
+        try {
+            filterChain.doFilter(requestToUse, responseToUse);
+        } finally {
+            doSaveAccessLog(requestToUse, responseToUse);
+            copyResponse(response);
+        }
+    }
+
+    private void doSaveAccessLog(final HttpServletRequest request,
+                                 final HttpServletResponse response) {
+        final String requestUri = request.getRequestURI();
+        final String requestHeaders = getRequestHeaders(request);
+        final String requestParams = getRequestParams(request);
+        final String requestString = getRequestString(request);
+        final String responseString = getResponseString(response);
+        final int responseStatus = response.getStatus();
+
+        final List<String> logs = new ArrayList<>();
+        logs.add("uri=" + requestUri);
+        logs.add("headers=" + requestHeaders);
+        logs.add("status=" + responseStatus);
+        logs.add("requestContentType=" + request.getContentType());
+        logs.add("responseContentType=" + response.getContentType());
+        logs.add("params=" + requestParams);
+        logs.add("request=" + requestString);
+        logs.add("response=" + responseString);
+
+        log.info(String.join(",", logs));
+    }
+
+    private String getRequestHeaders(final HttpServletRequest request) {
+        final Enumeration<String> headerNames = request.getHeaderNames();
+        final List<String> headers = new ArrayList<>();
+        while (headerNames.hasMoreElements()) {
+            final String key = headerNames.nextElement();
+            headers.add(key + ':' + request.getHeader(key));
+        }
+        return '[' + String.join(",", headers) + ']';
+    }
+
+    private String getRequestParams(final HttpServletRequest request) {
+        final Map<String, String[]> requestParams = request.getParameterMap();
+        final List<String> pairs = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(requestParams)) {
+            for (final Map.Entry<String, String[]> entry : requestParams.entrySet()) {
+                final String name = entry.getKey();
+                final String[] value = entry.getValue();
+                if (value == null) {
+                    pairs.add(name + "=");
+                } else {
+                    for (final String v : value) {
+                        pairs.add(name + "=" + v);
+                    }
+                }
+            }
+        }
+        String requestParamsStr = CollectionUtils.isEmpty(pairs) ? "" : String.join("&", pairs);
+        if (StringUtils.startsWithIgnoreCase(request.getContentType(), MediaType.APPLICATION_FORM_URLENCODED_VALUE)) {
+            requestParamsStr = URLDecoder.decode(requestParamsStr, StandardCharsets.UTF_8);
+        }
+        return requestParamsStr;
+    }
+
+    private String getRequestString(final HttpServletRequest request) {
+        final ContentCachingRequestWrapper wrapper = WebUtils.getNativeRequest(request, ContentCachingRequestWrapper.class);
+        if (wrapper != null) {
+            try {
+                final byte[] buf = wrapper.getContentAsByteArray();
+                return new String(buf, wrapper.getCharacterEncoding());
+            } catch (UnsupportedEncodingException e) {
+                return "[UNKNOWN]";
+            }
+        }
+        return "";
+    }
+
+    private String getResponseString(final HttpServletResponse response) {
+        final ContentCachingResponseWrapper wrapper = WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
+        if (wrapper != null) {
+            try {
+                final byte[] buf = wrapper.getContentAsByteArray();
+                return new String(buf, wrapper.getCharacterEncoding());
+            } catch (UnsupportedEncodingException e) {
+                return "[UNKNOWN]";
+            }
+        }
+        return "";
+    }
+
+    private void copyResponse(final HttpServletResponse response) {
+        final ContentCachingResponseWrapper wrapper = WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
+        if (wrapper != null) {
+            try {
+                wrapper.copyBodyToResponse();
+            } catch (IOException ignored) {
+            }
+        }
+    }
+}
--- spring-rest-log-request-response/src/main/java/cn/springcamp/springlogrequestresponse/LogInterceptorAdapter.java
+++ spring-rest-log-request-response/src/main/java/cn/springcamp/springlogrequestresponse/LogInterceptorAdapter.java
@@ -1,6 +1,5 @@
 package cn.springcamp.springlogrequestresponse;
 
-import jakarta.servlet.ServletRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +9,6 @@ import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import java.util.Arrays;
 import java.util.Map;
@@ -24,8 +22,7 @@ public class LogInterceptorAdapter implements HandlerInterceptor {
         if (DispatcherType.REQUEST.name().equals(request.getDispatcherType().name())
                 && request.getMethod().equals(HttpMethod.GET.name())) {
 
-            ServletRequest servletRequest = new ContentCachingRequestWrapper(request);
-            Map<String, String[]> params = servletRequest.getParameterMap();
+            Map<String, String[]> params = request.getParameterMap();
 
             // 从 request 中读取请求参数并打印
             params.forEach((key, value) -> log.info("logInterceptor " + key + "=" + Arrays.toString(value)));
