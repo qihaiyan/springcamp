a1f0beda5f8c540101295d6ab88132fa3975e3ff
qihaiyan <haiyan_qi@hotmail.com>
2025-08-03 15:02:35 +0800

    ut

--- spring-rest-log-request-response/src/test/java/cn/springcamp/springlogrequestresponse/DemoApplicationTest.java
+++ spring-rest-log-request-response/src/test/java/cn/springcamp/springlogrequestresponse/DemoApplicationTest.java
@@ -1,15 +1,18 @@
 package cn.springcamp.springlogrequestresponse;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class DemoApplicationTest {
